8ddbb7e937e2b89c1a0c37105a890188eacde0b4
pooja-s-nair <84776356+pooja-s-nair@users.noreply.github.com>
2021-06-19 23:52:06 +0530

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,38 +1,12 @@
-create table publisher(
-     id integer primary key,
-     name text,
-     country text
-);
 
-
-create  table books(
-     id integer primary key,
-     title text,
-     publisher integer,
-     foreign key (publisher)
-        references publisher(id)
-);
-
-
-create  table subjects(
-     id integer primary key,
-     name text
-);
+CREATE TABLE publisher(id integer PRIMARY KEY,name text,country text);
+CREATE TABLE books(id integer PRIMARY KEY,title text,publisher integer,FOREIGN KEY (publisher) REFERENCES publisher(id));
+CREATE TABLE subjects(id integer PRIMARY KEY,name text);
+CREATE TABLE books_subjects(book integer,subject integer,FOREIGN KEY (book) REFERENCES books(id),FOREIGN KEY (subject) REFERENCES subjects(id));  
 
 
 
-create  table books_subjects(
-     book integer,
-     subject integer,
-     foreign key(book)
-         references books(id),
-     foreign key(subject)
-         references subjects(id)
-);
-
-
-
-
+    
 
 
 
