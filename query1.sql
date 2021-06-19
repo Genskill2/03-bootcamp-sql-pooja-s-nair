8eb7f2badfe18817d22c07761904337af47fe328
pooja-s-nair <84776356+pooja-s-nair@users.noreply.github.com>
2021-06-19 23:49:08 +0530

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -1,22 +1,12 @@
-select title from books
-where publisher_id="1";
+
+
+select title
+from books
+where id in(
+	select id
+	from publisher
+	where name='PHI'
+);
 
 
 
-
-select * from books_subjects
-where 
-
-
-
-SELECT title ,(SELECT publisher FROM PUBLISHER WHERE publisher_Id=5) FROM BOOKS WHERE publisher_Id=5 ; 
-SELECT title ,(SELECT publisher FROM PUBLISHER WHERE publisher_Id=6) FROM BOOKS WHERE publisher_Id=6 ;
-
-
-
-select title , (select name from publisher where publisher_id in (select country from publisher where country="UK") )from books 
-
-
-
-select title , (select name from publisher where publisher_id=5) from books where  publisher_id=5 ; 
-select title , (select name from publisher where publisher_id=6) from books where  publisher_id=6 ; 
