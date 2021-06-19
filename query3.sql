abf9b9d0aac3584d16ab3ebc99e94c856ca5ea3e
pooja-s-nair <84776356+pooja-s-nair@users.noreply.github.com>
2021-06-19 23:57:01 +0530

    Delete query3.sql

--- query3.sql
+++
@@ -1,4 +0,0 @@
-select title from books where book_id in(select book_id from books_subjects where subjects_id in(select subjects_id from subjects where name ='Technology' or name='Politics'));
-
-
-
