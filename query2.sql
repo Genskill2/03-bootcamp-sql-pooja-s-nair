1cd05deca3850337a6c9f644dfcbd7e86d2c340b
pooja-s-nair <84776356+pooja-s-nair@users.noreply.github.com>
2021-06-19 23:56:42 +0530

    Delete query2.sql

--- query2.sql
+++
@@ -1,4 +0,0 @@
-
-select title ,(select name  from publisher where publisher_id=5) from books where publisher_id =5;
-select title ,(select name  from publisher where publisher_id=6) from books where publisher_id =6;
-
